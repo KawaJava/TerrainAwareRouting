691da7228b58a2ee1e3201aaa193bc0ba2c5d197
kawajava <kawajava0550@gmail.com>
2025-12-08 12:24:26 +0100

    Initial commit

---
+++ src/main/java/io/github/kawajava/TerrainAwareRouting/TerrainAwareRoutingApplication.java
@@ -0,0 +1,13 @@
+package io.github.kawajava.TerrainAwareRouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrainAwareRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrainAwareRoutingApplication.class, args);
+	}
+
+}
